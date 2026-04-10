1836ace37adf33f2c817ac166ab29b29a8b73550
Egor Myagkov <egor_myagkov_2006@mail.ru>
2026-04-10 21:58:51 +0300

    feat: add initial experiments and status constants

---
+++ src/constants/statuses.js
@@ -0,0 +1,7 @@
+export const STATUSES = {
+  PLAN: 'План',
+  IN_PROGRESS: 'В процессе',
+  COMPLETED: 'Завершён',
+};
+
+export const STATUS_LIST = [STATUSES.PLAN, STATUSES.IN_PROGRESS, STATUSES.COMPLETED];
